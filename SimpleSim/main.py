f2d42c3bd0e22d7ac5bf1c45e3c848bcf813d2ca
Aleks Mercik <aleks.mercik@outlook.com>
2016-11-28 08:36:09 -0500

    stuff

--- SimpleSim/main.py
+++ SimpleSim/main.py
@@ -2,27 +2,13 @@ import pygame
 import random
 from mapgen import mapManager
 import functions as f
+import renderer as render
 
 print(pygame.init())  ##(SUCCESS, FAILURE)
 
-COLOR = {
-    'WHITE': (255, 255, 255),
-    'BLACK': (0, 0, 0),
-    'RED': (255, 0, 0),
-    'GREEN': (0, 255, 0),
-    'BLUE': (0, 0, 255),
-    'YELLOW': (255, 255, 0)
-}
-
 mapXY = 800
 
-worldMap = mapManager(mapXY, 1000)
-
-minTemperature = min([min(i) for i in worldMap.temperatureMap ])
-maxTemperature = max([max(i) for i in worldMap.temperatureMap ])
-avgTemperature = ( minTemperature + maxTemperature ) / 2.0
-
-print(sum([ sum(i) for i in worldMap.temperatureMap]) / float(mapXY**2))
+worldMap = mapManager(mapXY, 1)
 
 clock = pygame.time.Clock()
 
@@ -43,63 +29,8 @@ while not gameExit:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print ("H:",worldMap.heightMap[event.pos[0]][event.pos[1]], "| M:",worldMap.moistureMap[event.pos[0]][event.pos[1]], "| T:",worldMap.temperatureMap[event.pos[0]][event.pos[1]], "| B:",worldMap.biomeMap[event.pos[0]][event.pos[1]])
 
-    mode = 'h'
-    
     if updateMap:
-        gameDisplay.fill(COLOR['BLACK'])
-        for x in range(mapXY):
-            #print(100*x/float(mapXY))
-            for y in range(mapXY):
-                if mode == 'h': ##---------------------------
-
-                    if worldMap.biomeMap[x][y] == 'water':
-                        gameDisplay.set_at((x, y), (0, 0, 200))
-                    elif worldMap.biomeMap[x][y] == 'frozenWater':
-                        gameDisplay.set_at((x, y), (100, 100, 200))
-                    elif worldMap.biomeMap[x][y] == 'glacier':
-                        gameDisplay.set_at((x, y), (255, 255, 255))
-                    elif worldMap.biomeMap[x][y] == 'snow':
-                        gameDisplay.set_at((x, y), (255, 255, 255))
-                    elif worldMap.biomeMap[x][y] == 'snowyMountain':
-                        gameDisplay.set_at((x, y), (255, 255, 255))
-                    elif worldMap.biomeMap[x][y] == 'mountain':
-                        gameDisplay.set_at((x, y), (120, 120, 120))
-                    elif worldMap.biomeMap[x][y] == 'permafrost':
-                        gameDisplay.set_at((x, y), (255, 255, 255))
-                    elif worldMap.biomeMap[x][y] == 'tundra':
-                        gameDisplay.set_at((x, y), (150, 150, 200))
-                    elif worldMap.biomeMap[x][y] == 'alpineTundra':
-                        gameDisplay.set_at((x, y), (100, 200, 150))
-                    elif worldMap.biomeMap[x][y] == 'taiga':
-                        gameDisplay.set_at((x, y), (0, 150, 100))
-                    elif worldMap.biomeMap[x][y] == 'boreal':
-                        gameDisplay.set_at((x, y), (0, 170, 0))
-                    elif worldMap.biomeMap[x][y] == 'grassPlains':
-                        gameDisplay.set_at((x, y), (80, 180, 20))
-                    elif worldMap.biomeMap[x][y] == 'savanna':
-                        gameDisplay.set_at((x, y), (90, 170, 0))
-                    elif worldMap.biomeMap[x][y] == 'wildPlains':
-                        gameDisplay.set_at((x, y), (100, 170, 0))
-                    elif worldMap.biomeMap[x][y] == 'barrenPlains':
-                        gameDisplay.set_at((x, y), (120, 190, 50))
-                    elif worldMap.biomeMap[x][y] == 'jungle':
-                        gameDisplay.set_at((x, y), (30, 100, 0))
-                    elif worldMap.biomeMap[x][y] == 'desert':
-                        gameDisplay.set_at((x, y), (255, 255, 150))
-                    else:
-                        gameDisplay.set_at((x, y), (255, 0, 255))
-                    
-                elif mode == 'm': ##-------------------------
-                    
-                    gameDisplay.set_at((x, y), 3*[f.fMap(worldMap.moistureMap[x][y], 0, 1, 0, 255)])
-                    
-                elif mode == 't': ##-------------------------
-                    
-                    if worldMap.temperatureMap[x][y] < avgTemperature:
-                        gameDisplay.set_at((x, y), (int(f.fMap(worldMap.temperatureMap[x][y], minTemperature, avgTemperature, 0, 255)),int(f.fMap(worldMap.temperatureMap[x][y], minTemperature, avgTemperature, 0, 255)), 255))
-                    else:
-                        gameDisplay.set_at((x, y), (255, int(f.fMap(worldMap.temperatureMap[x][y], avgTemperature, maxTemperature, 255, 0)), int(f.fMap(worldMap.temperatureMap[x][y], avgTemperature, maxTemperature, 255, 0))))
-
+        render.colorBiomes(gameDisplay, worldMap, 'h')
         pygame.display.update()
         updateMap = False
     
--- SimpleSim/mapgen.py
+++ SimpleSim/mapgen.py
@@ -1,6 +1,7 @@
 import noise
 import random
 import functions as f
+from time import sleep
 
 class mapManager:
 
@@ -21,6 +22,7 @@ class mapManager:
         self.moistureMap = []
         self.resourceMap = []
         self.biomeMap = []
+        self.foodMap = []
         self.generateMaps()
 
 
@@ -31,10 +33,16 @@ class mapManager:
         self.moistureMap = self.generateMoistureMap()
         print("Generating Temperature Map...")
         self.temperatureMap = self.generateTemperatureMap()
-        #self.heightMap=self.temperatureMap
-        #self.resourceMap = self.generateResourcemap()
+        print("Generating Resource Map...")
+        self.resourceMap = self.generateResourceMap()
         print("Generating Biome Map...")
         self.biomeMap = self.generateBiomeMap()
+        print("Generating Oceans...")
+        self.oceanMap()
+        print("Generating Rivers...")
+        self.riverMap()
+        print("Generating Terrain Bounty...")
+        self.generateFoodMap()
         print("Generation Complete.")
 
     
@@ -45,7 +53,7 @@ class mapManager:
         for x in range(self.mapSize):
             for y in range(self.mapSize):
                 absRange = min(self.mapSize/2.0, ((x-self.mapSize/2)**2 + (y-self.mapSize/2)**2)**0.5)
-                heightMultiplier = - ( absRange / (self.mapSize/2.0) )**12 + 1
+                heightMultiplier = - ( absRange / (self.mapSize/2.0) )**10 + 1
                 tempMap[x][y] *= heightMultiplier
         return tempMap
 
@@ -66,18 +74,18 @@ class mapManager:
         
         for x in range(self.mapSize):
             for y in range(self.mapSize):
-                tempMap[x][y] = tempMap[x][y] + f.fMap(y, 0, self.mapSize, -15, 40) + 0.7*f.fMap(self.moistureMap[x][y], 0, 1, 0, 10) + min(0, f.fMap(self.heightMap[x][y], 0, 1, 50, -30))
+                tempMap[x][y] = tempMap[x][y] + f.fMap(y, 0, self.mapSize, -10, 40) + 0.7*f.fMap(self.moistureMap[x][y], 0, 1, 0, 10) + min(0, f.fMap(self.heightMap[x][y], 0, 1, 50, -25))
             
         return tempMap
 
 
     def generateResourceMap(self):
         
-        tempMap = f.generateNoiseMap(self.moistureSeed, self.mapSize, 0.006, 0, 1, 5, 0.4, 1.5)
+        tempMap = f.generateNoiseMap(self.moistureSeed, self.mapSize, 0.05, 0, 1, 5, 0.4, 1.5)
         
         for x in range(self.mapSize):
             for y in range(self.mapSize):
-                tempMap[x][y] = tempMap[x][y]
+                tempMap[x][y] = tempMap[x][y] * (0.5*f.fMap(max(0, min(self.temperatureMap[x][y], 20)), 0, 20, 0, 1) + 0.5*f.fMap(max(20, min(self.temperatureMap[x][y], 30)), 20, 30, 1, 0))
             
         return tempMap
 
@@ -100,13 +108,15 @@ class mapManager:
                         tempMap[x][y] = 'permafrost'
                 elif self.temperatureMap[x][y] < 0:
                     if self.heightMap[x][y] < 0.5:
-                        tempMap[x][y] = 'frozenWater'
+                        tempMap[x][y] = 'frozenLake'
+                    elif self.moistureMap[x][y] < 0.7:
+                        tempMap[x][y] = 'snowyTaiga'
                     else:
                         tempMap[x][y] = 'snow'
                 elif self.heightMap[x][y] < 0.5:
-                    tempMap[x][y] = 'water'
+                    tempMap[x][y] = 'lake'
                 elif self.temperatureMap[x][y] < 5:
-                    if self.moistureMap[x][y] < 0.3:
+                    if self.moistureMap[x][y] < 0.5:
                         tempMap[x][y] = 'tundra'
                     elif self.moistureMap[x][y] < 0.6:
                         tempMap[x][y] = 'alpineTundra'
@@ -117,13 +127,18 @@ class mapManager:
                         tempMap[x][y] = 'alpineTundra'
                     else:
                         tempMap[x][y] = 'taiga'
+                elif self.temperatureMap[x][y] < 15:
+                    if self.moistureMap[x][y] < 0.3:
+                        tempMap[x][y] = 'barrenPlains'
+                    else:
+                        tempMap[x][y] = 'boreal'
                 elif self.temperatureMap[x][y] < 25:
                     if self.moistureMap[x][y] < 0.2:
                         tempMap[x][y] = 'barrenPlains'
                     elif self.moistureMap[x][y] < 0.4:
                         tempMap[x][y] = 'grassPlains'
                     else:
-                        tempMap[x][y] = 'boreal'
+                        tempMap[x][y] = 'temperate'
                 elif self.temperatureMap[x][y] < 27:
                     if self.moistureMap[x][y] < 0.3:
                         tempMap[x][y] = 'barrenPlains'
@@ -133,7 +148,7 @@ class mapManager:
                     if self.moistureMap[x][y] < 0.5:
                         tempMap[x][y] = 'wildPlains'
                     else:
-                        tempMap[x][y] = 'boreal'
+                        tempMap[x][y] = 'temperate'
                 elif self.temperatureMap[x][y] < 35:
                     if self.moistureMap[x][y] < 0.4:
                         tempMap[x][y] = 'wildPlains'
@@ -149,7 +164,129 @@ class mapManager:
                               
         return tempMap
 
+    def oceanMap(self):
+        checkPoints = [[0, 0]]
+        checked = [['' for i in range(self.mapSize)] for j in range(self.mapSize)]
+        while True:
+            if not checkPoints:
+                break
+            
+            cp = checkPoints.pop(0)
+            
+            if self.biomeMap[cp[0]][cp[1]] == 'frozenLake':
+                self.biomeMap[cp[0]][cp[1]] = 'frozenOcean'
+            elif self.biomeMap[cp[0]][cp[1]] == 'lake':
+                self.biomeMap[cp[0]][cp[1]] = 'ocean'
 
+            checked[cp[0]][cp[1]] = 'c'
+            
+            for xD in [-1, 0, 1]:
+                for yD in [-1, 0, 1]:
+                    
+                    if cp[0] + xD >= 0 and cp[0] + xD <= self.mapSize-1:
+                        if cp[1] + yD >= 0 and cp[1] + yD <= self.mapSize-1:
+                            if not checked[cp[0]+xD][cp[1]+yD] == 'c':
+                                if self.biomeMap[cp[0]+xD][cp[1]+yD] == 'glacier' or self.biomeMap[cp[0]+xD][cp[1]+yD] == 'frozenLake' or self.biomeMap[cp[0]+xD][cp[1]+yD] == 'lake':
+                                    checkPoints.append([cp[0]+xD, cp[1]+yD])
+                                    checked[cp[0]+xD][cp[1]+yD] = 'c'
+
+    def riverMap(self):
+        riverSeeds = []
+
+        for x in range(self.mapSize):
+            for y in range(self.mapSize):
+                if self.biomeMap[x][y] == 'mountain':
+                    if random.random() < 0.001:
+                        riverSeeds.append([x, y])
+
+        ind = 0
+        
+        while True:
+            if not riverSeeds:
+                break
+
+            low = 1
+            pos = []
+            isComplete = False
+            
+            for xD in [-1, 0, 1]:
+                for yD in [-1, 0, 1]:
+                    if isComplete:
+                        break
+                    
+                    if not (xD == 0 and yD == 0):
+                        if self.biomeMap[riverSeeds[0][0] + xD][riverSeeds[0][1] + yD] == 'river' or self.biomeMap[riverSeeds[0][0] + xD][riverSeeds[0][1] + yD] == 'riverLake':
+                            continue
+                        if self.heightMap[riverSeeds[0][0] + xD][riverSeeds[0][1] + yD] < 0.5:
+                            isComplete = True
+                            continue
+                        if self.heightMap[riverSeeds[0][0] + xD][riverSeeds[0][1] + yD] <= low:
+                            low = self.heightMap[riverSeeds[0][0] + xD][riverSeeds[0][1] + yD]
+                            pos = [riverSeeds[0][0] + xD,riverSeeds[0][1] + yD]
+
+            if self.heightMap[riverSeeds[0][0]][riverSeeds[0][1]] < low:
+                self.biomeMap[riverSeeds[0][0]][riverSeeds[0][1]] = 'riverLake'
+            else:
+                self.biomeMap[riverSeeds[0][0]][riverSeeds[0][1]] = 'river'
+
+            if pos == []:
+                isComplete = True
+
+            riverSeeds.pop(0)
+
+            if isComplete:
+                continue
+            
+            riverSeeds.append(pos)
+
+    def generateFoodMap(self):
+        tempMap = [[0 for i in range(self.mapSize)] for j in range(self.mapSize)]
+
+        for x in range(self.mapSize):
+            for y in range(self.mapSize):
+                foodCount = 0
+
+                if self.biomeMap[x][y] == 'lake':
+                    foodCount += 1
+                elif self.biomeMap[x][y] == 'river':
+                    foodCount += 1
+                elif self.biomeMap[x][y] == 'riverLake':
+                    foodCount += 1
+                elif self.biomeMap[x][y] == 'forzenLake':
+                    foodCount += 0.5
+                elif self.biomeMap[x][y] == 'frozenOcean':
+                    foodCount += 0.25
+                elif self.biomeMap[x][y] == 'ocean':
+                    foodCount += 1
+                elif self.biomeMap[x][y] == 'snow':
+                    foodCount += 0.1
+                elif self.biomeMap[x][y] == 'mountain':
+                    foodCount += 0.2
+                elif self.biomeMap[x][y] == 'tundra':
+                    foodCount += 0.25
+                elif self.biomeMap[x][y] == 'alpineTundra':
+                    foodCount += 0.5
+                elif self.biomeMap[x][y] == 'taiga':
+                    foodCount += 0.75
+                elif self.biomeMap[x][y] == 'snowyTaiga':
+                    foodCount += 0.5
+                elif self.biomeMap[x][y] == 'boreal':
+                    foodCount += 1
+                elif self.biomeMap[x][y] == 'temperate':
+                    foodCount += 1.5
+                elif self.biomeMap[x][y] == 'grassPlains':
+                    foodCount += 2
+                elif self.biomeMap[x][y] == 'savanna':
+                    foodCount += 0.75
+                elif self.biomeMap[x][y] == 'wildPlains':
+                    foodCount += 0.7
+                elif self.biomeMap[x][y] == 'barrenPlains':
+                    foodCount += 0.6
+                elif self.biomeMap[x][y] == 'jungle':
+                    foodCount += 1
+                elif self.biomeMap[x][y] == 'desert':
+                    foodCount += 0.1
+    
     def getSeed(self):
         return self.seed
 
